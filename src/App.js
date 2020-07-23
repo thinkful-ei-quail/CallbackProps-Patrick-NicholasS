72aa9b62113d80f6bd2c0f334d33d585284660db
Patrick Easter <epatri6@gmail.com>
2020-07-23 11:16:45 -0400

    Event Handlers Done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
 import List from './List'
 import './App.css';
+import STORE from './STORE';
 
 class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
+
+  state = {
+    store: STORE
+  }
+
+  handleAddClick = (listId) => {
+    const card = this.newRandomCard();
+    const {store} = this.state;
+    store.allCards.push(card);
+    const list = store.lists.filter(list => list.id === listId);
+    list.push(card);
+  }
+
+  handleDeleteClick = (id) => {
+    let {lists} = this.state;
+
+    lists = lists.map(list => {
+      return list.filter(card => {
+        return card.id !== id;
+      })
+    });
+
+    this.setState({lists: lists});
+  }
+
+  newRandomCard = () => {
+    const id = Math.random().toString(36).substring(2, 4)
+      + Math.random().toString(36).substring(2, 4);
+    return {
+      id,
+      title: `Random Card ${id}`,
+      content: 'lorem ipsum',
     }
-  };
+  }
 
   render() {
-    const { store } = this.props
+    const { store } = this.state
     return (
       <main className='App'>
         <header className='App-header'>
